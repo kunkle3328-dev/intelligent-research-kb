e3e11c954f7f76aa38af2203cc2f584f81418616
kunkle3328-dev <kunkle3328@gmail.com>
2026-03-16 03:14:48 -0400

    Create tailwind.config.js with content paths and custom theme configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customColor: '#1c1c1e',
+      },
+    },
+  },
+  plugins: [],
+};
